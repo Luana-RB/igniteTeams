0f8dfa54ba03686874b9699c48ed1a9fa11648d8
lrbassanesi.develcode <lrbassanesi@rd.com.br>
2024-12-23 09:35:52 -0300

    feat: project structure

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,8 +1,11 @@
-import { Groups } from "@screens/Groups";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
-  return <Groups />;
+export function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
